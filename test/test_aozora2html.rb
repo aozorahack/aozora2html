257f6c13d6bc29235ac2485511ec472c7bf40507
Ken Sato <ksato9700@gmail.com>
2015-08-16 09:22:23 +0900

    fix invalid multibyte char error in Ruby193

--- test/test_aozora2html.rb
+++ test/test_aozora2html.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'test_helper'
 require 'aozora2html'
 require 'fileutils'
--- test/test_inline_keigakomi_tag.rb
+++ test/test_inline_keigakomi_tag.rb
@@ -1,9 +1,10 @@
+# encoding: utf-8
 require 'test_helper'
 require 'aozora2html'
 
 class InlineKeigakomiTagTest < Test::Unit::TestCase
   def setup
-    stub(@parser).block_allowed_context?{true} 
+    stub(@parser).block_allowed_context?{true}
   end
 
   def test_keigakomi_new
