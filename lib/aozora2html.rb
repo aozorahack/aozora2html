499ea38a5445b80163e3097e305ca8eab2fb0954
takahashim <maki@rubycolor.org>
2017-02-27 15:35:16 +0900

    fix pathes

--- lib/aozora2html.rb
+++ lib/aozora2html.rb
@@ -2,8 +2,8 @@ require "aozora2html/version"
 require "aozora2html/zip"
 require "aozora2html/jis2ucs"
 require 't2hs'
-require 'embed_gaiji_tag'
-require 'accent_tag'
+require 'aozora2html/tag/embed_gaiji'
+require 'aozora2html/tag/accent'
 
 ## already defined in t2hs.rb
 class Aozora2Html
