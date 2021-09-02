49bdf8fee58ea1eecb046e5dd94b5ceb59e5524c
gemmaro <gemmaro.dev@gmail.com>
2021-09-03 08:07:01 +0900

    [ fix, change ] puts behavior for Ruby v3.0.x

--- lib/extensions.rb
+++ lib/extensions.rb
@@ -33,3 +33,12 @@ class String
   end
 end
 
+module Kernel
+  alias original_kernel_puts puts
+
+  def puts(*args)
+    original_kernel_puts(args)
+  rescue Encoding::CompatibilityError
+    original_kernel_puts(args.map { |arg| arg.force_encoding('utf-8') })
+  end
+end
