2bdd640adbb0ced14c2e73f26782c05c49063fe4
takahashim <takahashimm@gmail.com>
2021-12-15 02:12:30 +0900

    Jstream: load all content at first

--- lib/jstream.rb
+++ lib/jstream.rb
@@ -21,19 +21,20 @@ class Jstream
   # TODO: 将来的にはさすがにexitまではしないよう、仕様を変更する?
   def initialize(file_io)
     @line = 0
+    @col = 0
     @current_char = nil
     @file = file_io
 
     begin
-      tmp = @file.readline.chomp!("\r\n")
-      raise Aozora2Html::Error, Aozora2Html::I18n.t(:use_crlf) unless tmp
+      @lines = @file.readlines(LF)
+      unless @lines[0].end_with?(CRLF)
+        raise Aozora2Html::Error, Aozora2Html::I18n.t(:use_crlf)
+      end
     rescue Aozora2Html::Error => e
       puts e.message(1)
       if e.is_a?(Aozora2Html::Error)
         exit(2)
       end
-    ensure
-      @file.rewind
     end
   end
 
@@ -48,15 +49,16 @@ class Jstream
   #
   # TODO: EOFの場合はnilを返すように変更する?
   def read_char
-    char = @file.getc
+    char = getc
 
     if char == CR
-      char2 = @file.getc
+      char2 = getc
       if char2 != LF
         raise Aozora2Html::Error, Aozora2Html::I18n.t(:use_crlf)
       end
 
       @line += 1
+      @col = 0
       @current_char = char + char2
     elsif char.nil?
       @current_char = :eof
@@ -73,15 +75,16 @@ class Jstream
   # 行末の場合は(1文字ではなく)CR+LFを返す
   # 行末の先に進んだ場合の挙動は未定義になる
   def peek_char(pos)
-    original_pos = @file.pos
+    original_col = @col
+    original_line = @line
     char = nil
 
     begin
       pos.times { read_char }
 
-      char = @file.getc
+      char = getc
       if char == CR
-        char2 = @file.getc
+        char2 = getc
         if char2 != LF
           raise Aozora2Html::Error, Aozora2Html::I18n.t(:use_crlf)
         end
@@ -89,7 +92,8 @@ class Jstream
         char += char2
       end
     ensure
-      @file.seek(original_pos)
+      @col = original_col
+      @line = original_line
     end
 
     char
@@ -103,12 +107,24 @@ class Jstream
   #
   # 何も読み込む前は0、読み込み始めの最初の文字から\r\nまでが1、その次の文字から次の\r\nは2、……といった値になる
   def line
-    if @file.pos == 0
+    if @line == 0 && @col == 0
       0
     elsif @current_char == CRLF
       @line
     else
       @line + 1
     end
+  end
+
+  private
+
+  def getc
+    char = begin
+      @lines[@line][@col]
+    rescue StandardError
+      nil
+    end
+    @col += 1
+    char
   end
 end
